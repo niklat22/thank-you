e2a0ba3f2adf6f4fee1cbe196ac2cb3012b2355d
niklat22 <lathiyanikund@gmail.com>
2026-01-07 17:10:38 +0530

    Add Thank you component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ThankYouComponent } from './thank-you/thank-you.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: ThankYouComponent }
+];
---
+++ src/app/thank-you/thank-you.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thank-you',
+  imports: [],
+  templateUrl: './thank-you.component.html',
+  styleUrl: './thank-you.component.css'
+})
+export class ThankYouComponent {
+  message = 'Thank you welcome to Angular :).';
+}
